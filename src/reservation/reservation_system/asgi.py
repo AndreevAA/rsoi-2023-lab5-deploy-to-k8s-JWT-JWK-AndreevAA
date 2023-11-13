1e9177435afc81ff596e3d5c67bd47ada4245e19
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2023-11-14 00:14:09 +0300

    structure fix

--- src/reservation/reservation_system/asgi.py
+++ src/reservation/reservation_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gateway project.
+ASGI config for reservation_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gateway.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reservation_system.settings")
 
 application = get_asgi_application()
